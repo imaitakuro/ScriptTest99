26ea1904c4d5c623a82ebc4fcf3f1093fe51e153
imaitakuro <71498761+imaitakuro@users.noreply.github.com>
2020-09-24 17:07:27 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        int[] array = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i <= 4; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
+
+    // Update is called once per frame
+  
+
